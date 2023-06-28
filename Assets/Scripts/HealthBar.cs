612a5ffee808e25d37d696a02992e4037dfcbc84
KietHo1 <kiethostartup1999@gmail.com>
2023-06-28 20:48:52 +0700

    Part 26 - UI Health Bar

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider healthSlider;
+    public TMP_Text healthBarText;
+
+    Damageable playerDamageable;
+
+    private void Awake()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+        if(player == null)
+        {
+            Debug.Log("No player found in the scene. Make sure it has tag 'Player'");
+        }
+
+        playerDamageable = player.GetComponent<Damageable>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthSlider.value = CalculateSliderPercentage(playerDamageable.Health, playerDamageable.MaxHealth);
+        healthBarText.text = "HP " + playerDamageable.Health + " / " + playerDamageable.MaxHealth;
+    }
+
+    private void OnEnable()
+    {
+        playerDamageable.healthChanged.AddListener(onPlayerHealthChanged);
+    }
+
+    private void OnDisable()
+    {
+        playerDamageable.healthChanged.RemoveListener(onPlayerHealthChanged);
+    }
+
+    private float CalculateSliderPercentage(float currentHealth, float maxHealth)
+    {
+        return currentHealth / maxHealth;
+    }
+
+    private void onPlayerHealthChanged(int newHealth, int maxHealth)
+    {
+        healthSlider.value = CalculateSliderPercentage(newHealth, maxHealth);
+        healthBarText.text = "HP " + newHealth + " / " + maxHealth;
+    }
+}
